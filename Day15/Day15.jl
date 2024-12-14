14a6a6d6399167a8bb2d2bf65852bde3ac49302f
Christian Rick <christian@rick.se>
2024-12-14 21:29:57 +0100

    Updated gitignore

---
+++ Day15/Day15.jl
@@ -0,0 +1,17 @@
+function read_data(path::String)
+    return 
+end
+
+
+
+function part_one(data)
+    print("Part One: ")
+end
+
+function part_two(data)
+    print("Part Two: ")
+end
+
+
+#data = read_data("Day15-data.txt")
+data = read_data("Day15-test.txt")
